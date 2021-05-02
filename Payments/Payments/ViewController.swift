2845ec44f8e248d58ceba03e2324683384d95e8c
Nivedha Rajendran <nivedha.rajen@gmail.com>
2021-05-02 15:41:51 +0530

    Project files initial commit

---
+++ Payments/Payments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Payments
+//
+//  Created by Nivedha Rajendran on 02/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
